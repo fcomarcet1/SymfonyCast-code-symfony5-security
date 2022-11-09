6ada7a26674eb63875d3596ff7f3a33bf30a875d
fcomarcet1 <fcomarcet1@gmail.com>
2022-11-09 15:36:34 +0100

    Manual Authentication after registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Symfony\Component\Security\Http\Authenticator\FormLoginAuthenticator;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
@@ -18,7 +20,9 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        UserAuthenticatorInterface $userAuthenticator,
+        FormLoginAuthenticator $formLoginAuthenticator
     ): Response
     {
         $user = new User();
@@ -34,9 +38,18 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $userAuthenticator->authenticateUser(
+                $user,
+                $formLoginAuthenticator,
+                $request
+            );
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+
+            //add flash message
+            $this->addFlash('success', 'Your account has been created!');
 
             return $this->redirectToRoute('app_homepage');
         }
